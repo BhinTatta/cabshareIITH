5a599ad9425aefd3ad37ed5803870a23c6ed93d0
BhinTatta <bhintatta01@gmail.com>
2023-06-15 01:14:55 +0530

    added regex for mobile number verification

--- components/AddListing.jsx
+++ components/AddListing.jsx
@@ -48,6 +48,12 @@ const AddListing = () => {
     toast({ title: "Listing created successfully", status: "success" });
   };
 
+  const isValidPhoneNumber = (phoneNumber) => {
+    const phoneRegex = /^\d{10}$/; // Regular expression for a 10-digit phone number
+
+    return phoneRegex.test(phoneNumber);
+  };
+
   return (
     <Box
       w={{ base: "80%", md: "40%" }}
@@ -104,7 +110,8 @@ const AddListing = () => {
             date.length < 1 ||
             time.length < 1 ||
             phoneNumber.length < 1 ||
-            isLoading
+            isLoading ||
+            !isValidPhoneNumber(phoneNumber)
           }
           variantColor="teal"
           variant="solid"
